aba1aed3306fc9b4b23139a6d175ff5d8d8de5a9
zsitvat <tamaszsitva@gmail.com>
2022-08-27 21:51:11 +0200

    pdf mukodik

--- App/Controllers/TasksController.cs
+++ App/Controllers/TasksController.cs
@@ -13,7 +13,14 @@ using DocumentFormat.OpenXml.InkML;
 using System.Text;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
-using Font = iTextSharp.text.Font;
+using System.Web.WebPages;
+using Microsoft.Extensions.Hosting.Internal;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Grpc.Core;
+using Azure;
+using static System.Net.WebRequestMethods;
+using System.Security.AccessControl;
+using System.Diagnostics;
 
 namespace App.Controllers
 {
@@ -155,7 +162,7 @@ namespace App.Controllers
         }
 
 
-        //closedXML needed
+        //closedXML needed !!!!!
         public async Task<IActionResult> Excel()
         {
             var tasks = from u in _context.Tasks select u;
@@ -204,10 +211,8 @@ namespace App.Controllers
 
                 await using (var stream = new MemoryStream())
                 {
-          
                     workbook.SaveAs(stream);
                     var content = stream.ToArray();
-
                     return File(
                         content,
                         "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
@@ -215,12 +220,174 @@ namespace App.Controllers
                 }
             }
         }
-        //iTextSharp needed
-        public Task<IActionResult> PDF()
-        {
-            var dt = from u in _context.Tasks select u;
 
-            return null;
+        //iTextSharp needed !!!
+        public void PDF()
+        {
+            var tasks = from u in _context.Tasks select u;
+
+            if (tasks.Count() > 0)
+            {
+                int pdfRowIndex = 1;
+
+                string filename = "Tasks-" + DateTime.Now.ToString("dd-MM-yyyy");
+                string filepath = "/" + filename + ".pdf";
+
+                Document document = new Document(PageSize.A4, 5f, 5f, 10f, 10f);
+                FileStream fs = new FileStream(filepath, FileMode.Create);
+                PdfWriter writer = PdfWriter.GetInstance(document, fs);
+                document.Open();
+
+                Font font1 = FontFactory.GetFont(FontFactory.COURIER_BOLD, 8);
+                Font font2 = FontFactory.GetFont(FontFactory.COURIER, 6);
+
+                float[] columnDefinitionSize = { 1F, 1F, 1F, 1F };
+                PdfPTable table;
+                PdfPCell cell;
+
+                table = new PdfPTable(columnDefinitionSize)
+                {
+                    WidthPercentage = 100
+                };
+
+                cell = new PdfPCell
+                {
+                    BackgroundColor = new BaseColor(0xC0, 0xC0, 0xC0)
+                };
+
+                table.AddCell(new Phrase("Id", font1));
+                table.AddCell(new Phrase("Partner", font1));
+                table.AddCell(new Phrase("Leírás", font1));
+                table.AddCell(new Phrase("Átvétel helye", font1));
+                table.AddCell(new Phrase("Átvétel ideje", font1));
+                table.AddCell(new Phrase("Leadás helye", font1));
+                table.AddCell(new Phrase("Leadás ideje", font1));
+                table.AddCell(new Phrase("Egyéb megállóhelyek", font1));
+                table.AddCell(new Phrase("Rakomány ID", font1));
+                table.AddCell(new Phrase("Raktározás ideje", font1));
+                table.AddCell(new Phrase("Teljesítve", font1));
+                table.AddCell(new Phrase("Teljesítés ideje", font1));
+                table.AddCell(new Phrase("Késés", font1));
+                table.AddCell(new Phrase("Igért összeg", font1));
+                table.AddCell(new Phrase("Végleges összeg", font1));
+                table.AddCell(new Phrase("Büntetés összege", font1)); 
+                table.AddCell(new Phrase("Date", font1));
+                table.HeaderRows = 1;
+
+                foreach (Tasks task in tasks)
+                {
+                     if  (!task.Id.ToString().IsEmpty())
+                    {
+                        table.AddCell(new Phrase(task.Id.ToString(), font2));
+                    }else { table.AddCell(new Phrase("-", font2)); }
+
+                    if (!task.Partner.IsEmpty())
+                    {
+                        table.AddCell(new Phrase(task.Partner.ToString(), font2));
+                    }else { table.AddCell(new Phrase("-", font2)); }
+
+                    if (!task.Description.IsEmpty())
+                    {
+                        table.AddCell(new Phrase(task.Description.ToString(), font2));
+                    }else { table.AddCell(new Phrase("-", font2)); }
+
+                    if (!task.Place_of_receipt.IsEmpty())
+                    {
+                        table.AddCell(new Phrase(task.Place_of_receipt.ToString(), font2));
+                    }else { table.AddCell(new Phrase("-", font2)); }
+
+                    if (!task.Time_of_receipt.ToString().IsEmpty())
+                    {
+                        table.AddCell(new Phrase(task.Time_of_receipt.ToString(), font2));
+                    }else { table.AddCell(new Phrase("-", font2)); }
+
+                    if (!task.Place_of_delivery.IsEmpty())
+                    {
+                        table.AddCell(new Phrase(task.Place_of_delivery.ToString(), font2));
+
+                    }else { table.AddCell(new Phrase("-", font2)); }
+
+                    if (!task.Time_of_delivery.ToString().IsEmpty())
+                    {
+                        table.AddCell(new Phrase(task.Time_of_delivery.ToString(), font2));
+                    }else { table.AddCell(new Phrase("-", font2)); }
+
+                    if (!task.Other_stops.IsEmpty()) {  
+                        table.AddCell(new Phrase(task.Other_stops.ToString(), font2)) ;
+                    }else { table.AddCell(new Phrase("-", font2)); }
+
+                    if (!task.Id_cargo.IsEmpty())
+                    {
+                        table.AddCell(new Phrase(task.Id_cargo.ToString(), font2));
+                    } else { table.AddCell(new Phrase("-", font2)); }
+
+                    if (!task.Storage_time.IsEmpty())
+                    {
+                        table.AddCell(new Phrase(task.Storage_time.ToString(), font2));
+                    }else { table.AddCell(new Phrase("-", font2)); }
+
+                    if (!task.Completed.ToString().IsEmpty())
+                    {
+                        table.AddCell(new Phrase(task.Completed.ToString(), font2));
+                    }else { table.AddCell(new Phrase("-", font2)); }
+
+                    if (!task.Completion_time.ToString().IsEmpty())
+                    {
+                        table.AddCell(new Phrase(task.Completion_time.ToString(), font2));
+                    } else { table.AddCell(new Phrase("-", font2)); }
+
+                    if (!task.Time_of_delay.IsEmpty())
+                    {
+                        table.AddCell(new Phrase(task.Time_of_delay.ToString(), font2));
+                    }else { table.AddCell(new Phrase("-", font2)); }
+
+                    if (!task.Payment.IsEmpty())
+                    {
+                        table.AddCell(new Phrase(task.Payment.ToString(), font2));
+                    }else { table.AddCell(new Phrase("-", font2)); }
+
+                    if (!task.Final_Payment.IsEmpty())
+                    {
+                        table.AddCell(new Phrase(task.Final_Payment.ToString(), font2));
+                    }else { table.AddCell(new Phrase("-", font2)); }
+
+                    if (!task.Penalty.IsEmpty())
+                    {
+                        table.AddCell(new Phrase(task.Penalty.ToString(), font2));
+                    }else { table.AddCell(new Phrase("-", font2)); }
+
+                    if (!task.Date.ToString().IsEmpty())
+                    {
+                        table.AddCell(new Phrase(task.Date.ToString(), font2));
+                    }else { table.AddCell(new Phrase("-", font2)); }
+
+                    pdfRowIndex++;
+                }
+
+                document.Add(table);
+                document.Close();
+                document.CloseDocument();
+                document.Dispose();
+                writer.Close();
+                writer.Dispose();
+                fs.Close();
+                fs.Dispose();
+
+                FileStream sourceFile = new FileStream(filepath, FileMode.Open);
+                float fileSize = 0;
+                fileSize = sourceFile.Length;
+                byte[] getContent = new byte[Convert.ToInt32(Math.Truncate(fileSize))];
+                sourceFile.Read(getContent, 0, Convert.ToInt32(sourceFile.Length)); 
+                sourceFile.Close();
+                Response.Clear();
+                Response.Headers.Clear();
+                Response.ContentType = "application/pdf";
+                Response.Headers.Add("Content-Disposition", "attachment; filename=" + filename + ".pdf");
+                Response.Headers.Add("Content-Length", getContent.Length.ToString());
+                Response.Body.WriteAsync(getContent);
+                Response.Body.Flush();
+            }
         }
+
     }
 }
